aae970011e7891910626a9baf611596c78db58fa
Ziphil <yfe23457@nifty.com>
2021-02-19 16:01:37 +0900

    ポート番号を環境変数から取得するよう変更

--- source/variable.ts
+++ source/variable.ts
@@ -6,4 +6,5 @@ import dotenv from "dotenv";
 dotenv.config({path: "./variable.env"});
 
 export const VERSION = process.env["npm_package_version"] || "?";
+export const PORT = process.env["PORT"] || 3000;
 export const DISCORD_KEY = process.env["DISCORD_KEY"] || "dummy";
